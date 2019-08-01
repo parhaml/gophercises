2f99d97eca89943f387aeab41e72a58985d534e5
parhaml <lee.p.parham@gmail.com>
2019-08-01 16:22:01 -0500

    Add styling and ability to pass new templates

--- Ex3/story.go
+++ Ex3/story.go
@@ -21,31 +21,77 @@ var defaultHandlerTmpl = `
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Choose Your Own Adventure</title>
+	<title>Choose Your Own Adventure</title>
+	<style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
   </head>
 
   <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
+	<section class="page">
+    	<h1>{{.Title}}</h1>
+    	{{range .Paragraphs}}
+    	<p>{{.}}</p>
+    	{{end}}
 
-    <ul>
-    {{range .Options}}
-      <li><a href="/{{.Chapter}}">{{.Text}}</li>
-    {{end}}
-    </ul>
+    	<ul>
+    	{{range .Options}}
+    	  <li><a href="/{{.Chapter}}">{{.Text}}</li>
+    	{{end}}
+		</ul>
+	</page>
   </body>
 </html>
 `
 
 // NewHandler to run the story
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+func NewHandler(s Story, t *template.Template) http.Handler {
+	if t == nil {
+		t = tpl
+	}
+	return handler{s, t}
 }
 
 type handler struct {
 	s Story
+	t *template.Template
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
