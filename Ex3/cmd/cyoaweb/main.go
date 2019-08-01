49d68d4294805b0b811ab98f8884a75c2c664387
parhaml <lee.p.parham@gmail.com>
2019-08-01 15:14:05 -0500

    Finalize initial page display

--- Ex3/cmd/cyoaweb/main.go
+++ Ex3/cmd/cyoaweb/main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 	"fmt"
 	cyoa "gophercises/Ex3"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port on which to start the CYOA web application.")
 	filename := flag.String("file", "gopher.json", "JSON file in CYOA format")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -22,5 +25,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- Ex3/story.go
+++ Ex3/story.go
@@ -3,7 +3,54 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"text/template"
 )
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+
+  <body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+
+    <ul>
+    {{range .Options}}
+      <li><a href=""/{{.Chapter}}">{{.Text}}</li>
+    {{end}}
+    </ul>
+  </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 // JSONStory - Parse json format of a story to Story type
 func JSONStory(r io.Reader) (Story, error) {
